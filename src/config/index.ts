c10b22c51926243291324ce11e88a39df2ecf1bd
waihinsoe <waihinsoe332004@gmail.com>
2023-06-29 23:32:57 +0630

    feat: added github-auth and facebook-auth

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,15 @@
 interface Config {
   apiBaseUrl: string;
+  githubClientId: string;
+  githubClientSecret: string;
+  facebookClientId: string;
+  facebookClientSecret: string;
 }
 
 export const config: Config = {
   apiBaseUrl: process.env.NEXT_PUBLIC_API_BASE_URL || "",
+  githubClientId: process.env.GITHUB_ID || "",
+  githubClientSecret: process.env.GITHUB_SECRET || "",
+  facebookClientId: process.env.FACEBOOK_CLIENT_ID || "",
+  facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET || "",
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import AppProvider from "@/contexts/AppContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
-    <AppProvider>
-      <Component {...pageProps} />
-    </AppProvider>
+    <SessionProvider session={session}>
+      <AppProvider>
+        <Component {...pageProps} />
+      </AppProvider>
+    </SessionProvider>
   );
 }
